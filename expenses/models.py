8f31886326073197e013539913b7c66ef38fb990
rolczynska <alexandra.rolczynska@gmail.com>
2024-02-12 20:54:03 +0100

    Enable shared categories among users - Implement unique_together constraint in Category model

--- expenses/models.py
+++ expenses/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     class Meta:
         ordering = ('name',)
+        unique_together = ('name', 'user',)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.name}'
